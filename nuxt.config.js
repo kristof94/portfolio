d4dc651970677d464e4537d60984fe7b549232a5
kristof <christophe-dossantos@outlook.com>
2018-11-07 00:00:16 +0100

    debug rsponsive keyboar input

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     {
       name: 'viewport',
-      content: 'width=device-width, initial-scale=1'
+      content: 'width=device-width, initial-scale=1, user-scalable=false'
     },
     {
       hid: 'description',
