c34469c47592cfd0e236e37ef5475579b8d21d64
kristof <christophe-dossantos@outlook.com>
2018-11-06 22:29:53 +0100

    could send mail

--- api/routes/mail.js
+++ api/routes/mail.js
@@ -1,18 +1,9 @@
 import { Router } from 'express'
-import nodemailer from 'nodemailer'
+var apiKey = 'caf15914ff73a0cacd321c4ae8747727-4412457b-11b231c1'
+var domain = 'sandbox1f249708837846cda014eb902ffd44e7.mailgun.org'
+var mailgun = require('mailgun-js')({apiKey: apiKey, domain: domain})
 
 const router = Router()
-
-// Create a SMTP transport object
-var transport = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: 'kristof.dsantos@gmail.com',
-    pass: 'direct22'
-  }
-})
-
-console.log('SMTP Configured')
 
 // Message object
 
@@ -20,37 +11,22 @@ router.post('/mail', function (req, res, next) {
   const email = req.body.email
   const name = req.body.name
   const text = req.body.text
-  console.log(email)
-  console.log(name)
-  console.log(text)
   sendMail(email, name, text)
   res.sendStatus(200)
 })
 
 function sendMail (email, name, text) {
-  var message = {
-    // sender info
-    from: email,
-
-    // Comma separated list of recipients
+  var data = {
+    from: name + ' : ' + email,
     to: 'kristof.dsantos@gmail.com',
-
-    // Subject of the message
-    subject: 'portfolio - from ' + name + ' mail ' + email,
-
-    // plaintext body
+    subject: 'Hello',
     text: text
   }
-  transport.sendMail(message, function (error) {
-    if (error) {
-      console.log('Error occured')
-      console.log(error.message)
-      return
-    }
-    console.log('Message sent successfully!')
 
-    // if you don't want to use this transport object anymore, uncomment following line
-    // transport.close(); // close the connection pool
+  mailgun.messages().send(data, function (error, body) {
+    if (error) {
+      console.log(error.stack)
+    }
   })
 }
 
